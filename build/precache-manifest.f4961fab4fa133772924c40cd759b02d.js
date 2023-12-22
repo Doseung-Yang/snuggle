07515b29bb1e7feab8172127b970e27ab068caa4
unknown <doseung.yang@WADIZ.CO.KR>
2023-12-23 01:04:53 +0900

    test commit

---
+++ build/precache-manifest.f4961fab4fa133772924c40cd759b02d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071ad7ed5c72b8cceab991fb8f13eba8",
+    "url": "/snuggle/index.html"
+  },
+  {
+    "revision": "526878f51d73eba43cbd",
+    "url": "/snuggle/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "2be45297e3de015fa9db",
+    "url": "/snuggle/static/css/main.c145d942.chunk.css"
+  },
+  {
+    "revision": "526878f51d73eba43cbd",
+    "url": "/snuggle/static/js/2.4cb581f1.chunk.js"
+  },
+  {
+    "revision": "518ad85c942557c95d89f137c15069bc",
+    "url": "/snuggle/static/js/2.4cb581f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2708153651c5d91a2f24",
+    "url": "/snuggle/static/js/3.c73cc1a1.chunk.js"
+  },
+  {
+    "revision": "2be45297e3de015fa9db",
+    "url": "/snuggle/static/js/main.a2886fa1.chunk.js"
+  },
+  {
+    "revision": "af6d814757078ab28621",
+    "url": "/snuggle/static/js/runtime-main.6f0b036c.js"
+  },
+  {
+    "revision": "1c57ef705367c4142eaf5472dca151da",
+    "url": "/snuggle/static/media/Frame.1c57ef70.svg"
+  },
+  {
+    "revision": "ee157619016222095fdc08d54d74b710",
+    "url": "/snuggle/static/media/SyIcon.ee157619.svg"
+  },
+  {
+    "revision": "3ea3c3df6cc1b83afa15628bd58d574e",
+    "url": "/snuggle/static/media/catt.3ea3c3df.png"
+  },
+  {
+    "revision": "db18c72109edf21fd3d5c1bb45c4abf0",
+    "url": "/snuggle/static/media/image56.db18c721.png"
+  },
+  {
+    "revision": "52144c1bf56bf28c9668dca32efbd347",
+    "url": "/snuggle/static/media/pig.52144c1b.png"
+  },
+  {
+    "revision": "bbcccb68b82622814fd04a8a98454fa1",
+    "url": "/snuggle/static/media/rightSystemIcon.bbcccb68.svg"
+  },
+  {
+    "revision": "e65211ce360d6bd003bd8a4b529e2c15",
+    "url": "/snuggle/static/media/rightbtn.e65211ce.svg"
+  }
+]);
